c341f75fe4b609dd88807e95584b72ab71743042
hanhan.hu <hanhan.hu@hiero7.com>
2019-05-07 15:40:36 +0800

    add middleware token check

---
+++ app/Http/Middleware/TokenCheck.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Hiero7\Enums\PermissionError;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class TokenCheck
+{
+    public function handle($request, Closure $next)
+    {
+
+        try {
+
+            $token = JWTAuth::getToken();
+            JWTAuth::getPayload($token)->toArray();
+            return $next($request);
+        } catch (TokenExpiredException $e) {
+
+            return response()->json([
+                'message' => PermissionError::getDescription(PermissionError::TOKEN_EXPIRED),
+                'errorCode' => PermissionError::TOKEN_EXPIRED,
+            ])->setStatusCode(403);
+        } catch (TokenInvalidException $e) {
+
+            return response()->json([
+                'message' => PermissionError::getDescription(PermissionError::TOKEN_INVALID),
+                'errorCode' => PermissionError::TOKEN_INVALID,
+            ])->setStatusCode(403);
+        } catch (JWTException $e) {
+
+            return response()->json([
+                'message' => PermissionError::getDescription(PermissionError::TOKEN_ABSENT),
+                'errorCode' => PermissionError::TOKEN_ABSENT,
+            ])->setStatusCode(403);
+        }
+    }
+}
