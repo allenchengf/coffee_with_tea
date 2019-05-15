3f90125997eb7791755c1b78efda1bbcc1a1b9a1
allenchen <allen.chen@hiero7.com>
2019-05-15 15:03:19 +0800

    add locationnetwork table seeder

---
+++ database/seeds/LocationNetworkTableSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LocationNetworkTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => 1,
+                'continent_id' => 1,
+                'country_id' => 2,
+                'location' => 'All',
+                'isp' => 'All',
+                'network_id' => 2,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 2,
+                'continent_id' => 4,
+                'country_id' => 1,
+                'location' => 'All',
+                'isp' => 'All',
+                'network_id' => 1,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 3,
+                'continent_id' => 4,
+                'country_id' => 1,
+                'location' => 'All',
+                'isp' => 'Dian xin',
+                'network_id' => 3,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 4,
+                'continent_id' => 4,
+                'country_id' => 1,
+                'location' => 'All',
+                'isp' => 'Lian tong',
+                'network_id' => 4,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 5,
+                'continent_id' => 1,
+                'country_id' => 2,
+                'location' => 'All',
+                'isp' => 'All',
+                'network_id' => 15,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 6,
+                'continent_id' => 4,
+                'country_id' => 1,
+                'location' => 'All',
+                'isp' => 'All',
+                'network_id' => 16,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 7,
+                'continent_id' => 4,
+                'country_id' => 1,
+                'location' => 'Beijing',
+                'isp' => 'Yidong',
+                'network_id' => 101,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ], [
+                'id' => 8,
+                'continent_id' => 4,
+                'country_id' => 1,
+                'location' => 'Beijing',
+                'isp' => 'Dian xin',
+                'network_id' => 33,
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ]
+        ];
+
+        foreach ($data as $key => $value) {
+            DB::table('location_networks')->insert($value);
+        }
+    }
+}
