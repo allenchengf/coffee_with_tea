dafa687668ac9a9e5763fbfa3ec9ad12d4e46513
yuan.kao <yuan.kao@hiero7.com>
2019-05-29 09:33:11 +0800

    rewrite Test.

--- tests/Unit/LocationDnsTest.php
+++ tests/Unit/LocationDnsTest.php
@@ -2,15 +2,14 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use Hiero7\Services\LocationDnsSettingService;
 use Hiero7\Repositories\LineRepository;
-use Hiero7\Services\DnsProviderService;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Http\Request;
 use Hiero7\Repositories\LocationDnsSettingRepository;
+use Hiero7\Services\DnsProviderService;
+use Hiero7\Services\LocationDnsSettingService;
+use Hiero7\Models\LocationNetwork;
+use Hiero7\Models\{Domain,LocationDnsSetting};
 use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
 
 class LocationDnsTest extends TestCase
 {
@@ -21,9 +20,12 @@ class LocationDnsTest extends TestCase
         Artisan::call('db:seed');
         $this->seed('LocationDnsSettingSeeder');
         $this->dnsprovider = $this->initMock(DnsProviderService::class);
-        app()->call([$this, 'repository']);  
-        app()->call([$this,'dnsPodMock']);  
-        $this->service = new LocationDnsSettingService($this->locationDnsSettingRepository,$this->dnsprovider,$this->lineRepository);
+        app()->call([$this, 'repository']);
+        app()->call([$this, 'dnsPodMock']);
+        $this->domain = Domain::inRandomOrder()->first();
+        $this->locationNetwork = LocationNetwork::inRandomOrder()->first();
+        $this->locationDnsSetting = LocationDnsSetting::first();
+        $this->service = new LocationDnsSettingService($this->locationDnsSettingRepository, $this->dnsprovider, $this->lineRepository);
 
     }
 
@@ -36,23 +38,23 @@ class LocationDnsTest extends TestCase
     public function dnsPodMock()
     {
         $this->dnsprovider->shouldReceive('createRecord')->withAnyArgs()
-                                ->andReturn(["message" =>"Success","errorCode"=>null,"data" => [
-                                            "record" => [
-                                                "id" => "426118405",
-                                                "name" => "hiero7.test1.com",
-                                                "status" => "enabled",
-                                                "weight" => null
-                                    ]]]);
+            ->andReturn(["message" => "Success", "errorCode" => null, "data" => [
+                "record" => [
+                    "id" => "426118405",
+                    "name" => "hiero7.test1.com",
+                    "status" => "enabled",
+                    "weight" => null,
+                ]]]);
         $this->dnsprovider->shouldReceive('editRecord')->withAnyArgs()
-                                ->andReturn(["message" =>"Success","errorCode"=>null,"data" => [
-                                        "record" => [
-                                            "id" => "426278576",
-                                            "name" => "hiero7.test1.com",
-                                            "value" => "cCnPjg.com.",
-                                            "status" => "enable",
-                                            "weight" => null
-                                    ]]]);
-}
+            ->andReturn(["message" => "Success", "errorCode" => null, "data" => [
+                "record" => [
+                    "id" => "426278576",
+                    "name" => "hiero7.test1.com",
+                    "value" => "cCnPjg.com.",
+                    "status" => "enable",
+                    "weight" => null,
+                ]]]);
+    }
 
     public function tearDown()
     {
@@ -60,70 +62,46 @@ class LocationDnsTest extends TestCase
         parent::tearDown();
     }
 
-    public function testFormatCreateRecord()
-    {
-        $domainId = 1;
-        $locationNetworkRid = 1;
-
-        $data = [
-            "continent_id" => 1,
-            "country_id" => 2,
-            "network_id" => 2,
-            "cdn_id" => 2,
-            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10"
-        ];
-
-        $result = $this->service->formatData($data,$domainId,$locationNetworkRid,'create');
-
-        $response = [
-            "domain_id" => $domainId,
-            "domain_cname" => "hiero7.test1.com",
-            "cdn_id" => 2,
-            "cdn_cname" => "dnspod.com",
-            "network_name" => "国外",
-            "location_networks_id" => $locationNetworkRid,
-            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10"
-        ];
-
-        $this->assertEquals($result,$response);
-    }
-
     public function testCreateRecord()
     {
-        $domain = 1;
-        $locationNetworkRid = 1;
-
         $data = [
-            "continent_id" => 1,
-            "country_id" => 2,
-            "network_id" => 2,
-            "cdn_id" => 2,
-            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10"
+            "cdn_id" => $this->domain->cdns()->first()->id,
+            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10",
         ];
 
-        $response = $this->service->createSetting($data,$domain,$locationNetworkRid);
+        $response = $this->service->createSetting($data, $this->domain, $this->locationNetwork);
 
-        $this->assertEquals($response,true);
+        $this->assertEquals($response, true);
 
     }
 
-    public function testUpdateSetting()
+    public function testUpdateDataNotExist()
     {
-        $domain = 1;
-        $locationNetworkRid = 1;
-
         $data = [
-            "continent_id" => 1,
-            "country_id" => 2,
-            "network_id" => 2,
-            "cdn_id" => 2,
-            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10"
+            "cdn_id" => $this->domain->cdns()->first()->id,
+            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10",
         ];
 
-        $response = $this->service->updateSetting($data,$domain,$locationNetworkRid);
+        $response = $this->service->updateSetting($data, $this->domain, $this->locationNetwork);
 
-        $this->assertEquals($response,true);
+        $this->assertEquals($response, false);
+
+    }
+
+    public function testUpdateDataExist()
+    {   
+        $this->domain = new Domain;
+        $this->domain = $this->domain->where('id',$this->locationDnsSetting->domain_id)->first();
+        $this->locationNetwork = new LocationNetwork;
+        $this->locationNetwork = $this->locationNetwork->where('id',$this->locationDnsSetting->location_networks_id)->first();
+        $data = [
+            "cdn_id" => $this->domain->cdns()->first()->id,
+            "edited_by" => "de20afd0-d009-4fbf-a3b0-2c3257915d10",
+        ];
+
+        $response = $this->service->updateSetting($data, $this->domain, $this->locationNetwork);
+
+        $this->assertEquals($response, true);
 
     }
 }
-
