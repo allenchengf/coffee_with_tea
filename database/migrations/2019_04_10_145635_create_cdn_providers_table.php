4f31aad18115ac0ea9612c51742a1459f269eb0b
allenchen <allen.chen@hiero7.com>
2019-06-10 15:41:13 +0800

    add cdn providers controller, model, migration

---
+++ database/migrations/2019_04_10_145635_create_cdn_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCdnProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cdn_providers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum('status', ['active','stop']);
+            $table->integer('ttl')->nullable();
+            $table->uuid('edited_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cdn_providers');
+    }
+}
