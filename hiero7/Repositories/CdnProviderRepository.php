349b11ebef9f4cef63c6a1ae0f76a32fc04888e3
allenchen <allen.chen@hiero7.com>
2019-06-24 09:15:20 +0800

    modify change status method logic

--- hiero7/Repositories/CdnProviderRepository.php
+++ hiero7/Repositories/CdnProviderRepository.php
@@ -11,7 +11,7 @@ namespace Hiero7\Repositories;
 
 use Hiero7\Models\Cdn;
 use Hiero7\Models\CdnProvider;
-
+use \DB;
 class CdnProviderRepository
 {
     protected $cdnProvider;
@@ -26,5 +26,16 @@ class CdnProviderRepository
     public function getCdnProvider(int $ugid)
     {
         return $this->cdnProvider::where('user_group_id', $ugid)->orderBy('created_at', 'asc')->get();
+    }
+
+    public function getDefault($domainId)
+    {
+        $results = DB::table('cdns')
+                    ->join('cdn_providers', 'cdns.cdn_provider_id', '=', 'cdn_providers.id')
+                    ->select('default')
+                    ->where('domain_id', $domainId)
+                    ->where('cdn_providers.status', 'active')
+                    ->get()->pluck('default')->all();
+        return $results;
     }
 }
