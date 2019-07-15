75dab3feaea51df5dffb1465b92ec0a7afe44300
allenchen <allen.chen@hiero7.com>
2019-07-15 15:41:41 +0800

    add openrationlog request

--- app/Http/Controllers/Api/v1/OperationLogController.php
+++ app/Http/Controllers/Api/v1/OperationLogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OperationLogRequest;
 use Hiero7\Services\OperationLogService;
 use Hiero7\Traits\OperationLogTrait;
 use Illuminate\Http\Request;
@@ -23,7 +24,7 @@ class OperationLogController extends Controller
         $this->operationLogService = $operationLogService;
     }
 
-    public function index(Request $request)
+    public function index(OperationLogRequest $request)
     {
         return $this->operationLogService->get($request);
     }
