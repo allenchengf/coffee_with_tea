14f0d572893a3c5f6af1f58673217fa6e75b744d
lin.leo <lin.leo@hiero7.com>
2019-08-23 17:03:55 +0800

    Modify scan logs 順序

--- database/migrations/2019_08_22_173205_create_scan_log.php
+++ database/migrations/2019_08_22_173205_create_scan_log.php
@@ -15,12 +15,12 @@ class CreateScanLog extends Migration
     {
         Schema::create('scan_logs', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('cdn_provider_id')->unsigned();
             $table->integer('scan_platform_id')->unsigned();
             $table->integer('location_network_id')->unsigned();
+            $table->integer('cdn_provider_id')->unsigned();
             $table->string('latency', 10)->nullable();
-            $table->foreign('cdn_provider_id')->references('id')->on('cdn_providers')->onDelete('cascade');
             $table->foreign('scan_platform_id')->references('id')->on('scan_platforms')->onDelete('cascade');
+            $table->foreign('cdn_provider_id')->references('id')->on('cdn_providers')->onDelete('cascade');
             $table->foreign('location_network_id')->references('id')->on('location_networks')->onDelete('cascade');
             $table->timestamps();
         });
