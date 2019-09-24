1d635d0da698a5c4ca343a4cf8a4cc9ec3159a90
justin.hsu <justin.hsu@hiero7.com>
2019-09-24 11:40:25 +0800

    edit APIs: scan.create & scan.show for adding scanned_at key of response

--- hiero7/Services/ScanProviderService.php
+++ hiero7/Services/ScanProviderService.php
@@ -259,7 +259,7 @@ class ScanProviderService
             $this->create($scanneds, $cdnProvider->id, $scanPlatform->id, $created_at);
         }
 
-        return $scanneds;
+        return [$scanneds, $created_at];
     }
 
     /**
