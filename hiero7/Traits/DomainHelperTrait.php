33926075dcbde4614f080943836163309ce56884
lin.leo <lin.leo@hiero7.com>
2019-09-25 15:57:22 +0800

    Modify Domain CNAME format Method

--- hiero7/Traits/DomainHelperTrait.php
+++ hiero7/Traits/DomainHelperTrait.php
@@ -12,26 +12,26 @@ trait DomainHelperTrait
 
     /**
      * 調整 Domain->cname 格式
-     * 
+     *
      * 如果 DNS Pod 為付費版本，
      * 會保留原本格式
-     * 
+     *
      * 後尾再加上 {user_group_id}
-     * 
+     *
      * CNAME = www.iroute.com
      * 免費版本 => wwwiroutecom.{user_group_id}
      * 付費版本 => www.iroute.com.{user_group_id}
-     * 
+     *
      * 輸出時都會轉為小寫
-     * 
+     *
      * @param string $cname
      * @param integer $ugid
      * @return string
      */
     public function formatDomainCname(string $cname, int $ugid): string
     {
-        if (env('SCHEME') == 1) {
-            return preg_replace("/[[:punct:]]/i", '', $cname);
+        if (env('SCHEME', 1) == 1) {
+            $cname = preg_replace("/[[:punct:]]/i", '', $cname);
         }
 
         return strtolower($cname . '.' . $ugid);
